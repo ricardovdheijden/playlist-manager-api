681b06385316af9b27ec7100e3524e88e1dba908
ricardovdheijden <ricardovdheijden@hotmail.com>
2019-06-15 17:56:21 +0200

    initial commit Spring Boot

---
+++ src/main/java/nl/ricardovdheijden/playlistmanagerapi/PlaylistManagerApiApplication.java
@@ -0,0 +1,13 @@
+package nl.ricardovdheijden.playlistmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistManagerApiApplication.class, args);
+	}
+
+}
